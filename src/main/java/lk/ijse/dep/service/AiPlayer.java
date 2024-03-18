4a41ef0a9bb6f251f6910c8f6214a8341175a334
gihanviduranga <gihanvidu123@gmai.com>
2024-03-18 21:26:02 +0530

    init the project

--- src/main/java/lk/ijse/dep/service/AiPlayer.java
+++ src/main/java/lk/ijse/dep/service/AiPlayer.java
@@ -3,37 +3,94 @@ package lk.ijse.dep.service;
 import java.util.Random;
 
 public class AiPlayer extends Player {
-    public AiPlayer() {
-        super();
-    }
+    public AiPlayer(Board newBoard) {
+        super(newBoard);
 
-    public AiPlayer(Board board) {
-        super(board);
+
     }
 
     @Override
     public void movePiece(int col) {
-        do {
-            col = (int) (Math.random() * 6);
 
 
-        } while (!board.isLegalMove(col));
+        int x = colChosser();
+        if (x == -1) {
 
-        if (board.isLegalMove(col)){
-            board.updateMove(col, Piece.GREEN);
-            board.getBoardUI().update(col, false);
 
-            if (board.findWinner().getWinningPiece().equals(Piece.EMPTY)){
-                if (!board.existsLegalMoves()){
-                    board.getBoardUI().notifyWinner(new Winner(Piece.EMPTY));
-                }
-            }else {
-                board.getBoardUI().notifyWinner(board.findWinner());
-            }
+            do {
+
+                //  Random rand = new Random();
+                //  col = rand.nextInt(6);
+                int range = 6;
+                col = (int) (Math.random() * range);
+
+            } while (!board.isLegalMove(col));
+        } else {
+
+            col = x;
         }
+
+
+        board.updateMove(col, Piece.GREEN);
+        board.getBoardUI().update(col, false);
+
+        if (board.findWinner().getWinningPiece() != (Piece.EMPTY)) {
+            board.getBoardUI().notifyWinner(board.findWinner());
+
+
+        } else {
+
+            if (!board.existsLegalMoves()) board.getBoardUI().notifyWinner(new Winner(Piece.EMPTY));
+
+
+        }
+
 
     }
 
+    private int colChosser() {
+
+        for (int i = 0; i < 6; i++) {
+
+            if (board.isLegalMove(i)) {
+                int row = board.findNextAvailableSpot(i);
+                board.updateMove(i, Piece.GREEN);
+                if (board.findWinner().getWinningPiece() == Piece.GREEN) {
+
+                    board.updateMove(i, row, Piece.EMPTY);
+                    return i;
+                } else {
+                    board.updateMove(i, row, Piece.EMPTY);
 
 
+                }
+
+
+            }
+
+        }
+
+        for (int i = 0; i < 6; i++) {
+
+            if (board.isLegalMove(i)) {
+                int row = board.findNextAvailableSpot(i);
+                board.updateMove(i, Piece.BLUE);
+                if (board.findWinner().getWinningPiece() == Piece.BLUE) {
+
+                    board.updateMove(i, row, Piece.EMPTY);
+                    return i;
+                } else {
+                    board.updateMove(i, row, Piece.EMPTY);
+
+
+                }
+
+
+            }
+
+        }
+        return -1;
+
+
+    }
 }
